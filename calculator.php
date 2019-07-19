60cfccd7097a916f2c7fae3ff0b2a0a90aca5212
TreSach <35347069+TreSach@users.noreply.github.com>
2019-07-19 01:21:04 -0400

    Justify centered the operator

--- calculator.php
+++ calculator.php
@@ -259,7 +259,7 @@ form{
 
 input[type="radio"] {
    display:none;
-   // visibility: hidden;
+  
 }
 
 input[type="text"] {
@@ -272,6 +272,7 @@ label {
     border: 1px solid black;
     background-color: yellow;
     color: black;
+    font-size: 20px;
 }
 
 .selected {
@@ -292,7 +293,11 @@ label {
   display: flex;
   flex-wrap: wrap;
   flex: 0 1 100%;
- 
+ justify-content: center;
+}
+
+.small {
+  font-size: 14px;
 }
 
 label, input[type="submit"] {
@@ -304,6 +309,7 @@ label, input[type="submit"] {
 }
 
 input[type="submit"]{
+  flex: 0 1 20%;
   border: 0;
   background-color: black;
   color: white;
@@ -380,11 +386,11 @@ input[type="submit"]{
      
      
       <div class="btn">
-      <input type="radio" value="x^2"name="op" id="x^2"><label for="x^2">x^2</label>
+      <input type="radio" value="x^2"name="op" id="x^2"><label for="x^2">x<sup class="small">2</sup></label>
     
-     <input type="radio" value="x^y"name="op" id="x^y"><label for="x^y">x^y</label>
+     <input type="radio" value="x^y"name="op" id="x^y"><label for="x^y">x<sup class="small">y</sup></label>
   
-     <input type="radio" value="e^x"name="op" id="e^x"><label for="e^x">e^x</label>
+     <input type="radio" value="e^x"name="op" id="e^x"><label for="e^x">e<sup class="small">x</sup></label>
     
      <input type="radio" value="sin"name="op" id="sin"><label for="sin">sin</label>
      
@@ -394,15 +400,15 @@ input[type="submit"]{
 
 
      <div class="btn">
-     <input type="radio" value="log10"name="op" id="log10"><label for="log10">log10</label>
+     <input type="radio" value="log10"name="op" id="log10"><label for="log10">log<sub class="small">10</sub></label>
      <input type="radio" value="ln"name="op" id="ln"><label for="ln">ln</label>
      <input type="radio" value="pi"name="op" id="&pi;"><label for="&pi;">&pi;</label>
      
-     <input type="radio" value="sin^-1"name="op" id="sin^-1"><label for="sin^-1">sin^-1</label>
+     <input type="radio" value="sin^-1"name="op" id="sin^-1"><label for="sin^-1">sin<sup class="small">-1</sup></label>
      
-     <input type="radio" value="cos^-1"name="op" id="cos^-1"><label for="cos^-1">cos^-1</label>
+     <input type="radio" value="cos^-1"name="op" id="cos^-1"><label for="cos^-1">cos<sup class="small">-1</sup></label>
      
-     <input type="radio" value="tan^-1"name="op" id="tan^-1"><label for="tan^-1">tan^-1</label>
+     <input type="radio" value="tan^-1"name="op" id="tan^-1"><label for="tan^-1">tan<sup class="small">-1</sup></label>
      </div>
 
 
@@ -410,7 +416,7 @@ input[type="submit"]{
      <input type="radio" value="sqrt"name="op" id="sqrt"><label for="sqrt">&#8730;</label>
      <input type="radio" value="cubert"name="op" id="cubert"><label for="cubert">&#8731;</label>
 
-     <input type="radio" value="nthrt"name="op" id="nthrt"><label for="nthrt">x&#8730;</label>
+     <input type="radio" value="nthrt"name="op" id="nthrt"><label for="nthrt"><sup class="small">x</sup>&#8730;</label>
      
      <input type="radio" value="sinh"name="op" id="sinh"><label for="sinh">sinh</label>
      
@@ -422,11 +428,11 @@ input[type="submit"]{
 
      <div class="btn">
      <input type="radio" value="+/-"name="op" id="+/-"><label for="+/-">+/-</label>
-     <input type="radio" value="sinh^-1"name="op" id="sinh^-1"><label for="sinh^-1">sinh^-1</label>
+     <input type="radio" value="sinh^-1"name="op" id="sinh^-1"><label for="sinh^-1">sinh<sup class="small">-1</sup></label>
      
-     <input type="radio" value="cosh^-1"name="op" id="cosh^-1"><label for="cosh^-1">cosh^-1</label>
+     <input type="radio" value="cosh^-1"name="op" id="cosh^-1"><label for="cosh^-1">cosh<sup class="small">-1</sup></label>
      
-     <input type="radio" value="tanh^-1"name="op" id="tanh^-1"><label for="tanh^-1">tanh^-1</label>
+     <input type="radio" value="tanh^-1"name="op" id="tanh^-1"><label for="tanh^-1">tanh<sup class="small">-1</sup></label>
    
      <input type="submit" value="=">
      </div>
@@ -434,8 +440,7 @@ input[type="submit"]{
 
 
   </form>
-  <br><br>
-
+  
   <!--<div class="notice">
   NOTE: If you use an operator that does not require adding second input (i.e., x^2, sin, +/-), please only input number in the left hand text box and the operator.
   This application is still working in progress
